3489c1e3bb8df8c4a3561518c5f3f428889e59a1
Serkan Sipahi <serkan.sipahi@yahoo.de>
2015-01-30 22:53:11 +0100

    proof of concept

--- src/consistentity.js
+++ src/consistentity.js
@@ -1,6 +1,21 @@
 
-class Consistentity {
-	constructor(path=null, entity=null, config=null){
-		
+
+class ConsistEntity {
+	constructor(accessor={}, entities=[{}]){
+        this.entities = [];
+        this.init(entities);
 	}
+    init(entities)
+        entities.forEach(function(entity, _){
+            ConsistEntityModel.prototype = Object.create(accessor);
+            this.entities.push(new ConsistEntityModel(entity));
+        });
+    }
+    getEntities(){
+        return this.entities;
+    }
+};
+
+function ConsistEntityModel(entity){
+    this.entity = entity;
 }
