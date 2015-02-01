3922143d06e39dcfe9e3efff9b2153aec6761633
Serkan Sipahi <serkan.sipahi@yahoo.de>
2015-02-01 19:44:30 +0100

    removed unused doc types

--- src/consistentity.js
+++ src/consistentity.js
@@ -1,22 +1,54 @@
-
+/**
+ * @author Serkan Sipahi
+ * @email serkan.sipahi@yahoo.de
+ * @license MIT license
+ */
 export default class ConsistEntity {
+    /**
+     * @param  {Object} accessor
+     * @param  {Array} entities
+     * @return {undefined}
+     */
     constructor(accessor={}, entities=[{}]){
-        this.entities = entities;
+        this.tmpEntities = entities;
         this.entitiesModels = [];
         this.init();
     }
+    /**
+     * @return {undefined}
+     */
     init(){
-        this.entities.forEach((entity, _) => {
+        this.createEntitiesModels();
+        this.deleteTmpEntities();
+    }
+    /**
+     * @return {undefined}
+     */
+    createEntitiesModels(){
+        this.tmpEntities.forEach((entity, _) => {
             ConsistEntityModel.prototype = Object.create(accessor);
             this.entitiesModels.push(new ConsistEntityModel(entity));
         });
-        this.entities = [{}];
     }
+    /**
+     * @return {undefined}
+     */
+    deleteTmpEntities(){
+        this.tmpEntities = [{}];
+    }
+    /**
+     * [getDatas description]
+     * @return {Object} this.entitiesModels
+     */
     getDatas(){
         return this.entitiesModels;
     }
 };
 
+/**
+ * @param {Object} entitiy
+ * @return {Instance} of ConsistEntityModel
+ */
 function ConsistEntityModel(entity){
-    this.entity = entity;
+    this.entity = entity || {};
 }
